9b4646cdefd0b56a98fbf22d13dd8251a8ef3077
Hyunoh_Na <ohmap@naver.com>
2025-03-28 19:57:35 +0900

    3/28

--- src/collection/map/test/DictionaryTest.java
+++ src/collection/map/test/DictionaryTest.java
@@ -11,11 +11,11 @@ public class DictionaryTest {
         Scanner sc = new Scanner(System.in);
 
         Map<String, String> map = new HashMap<>();
+        System.out.println("==단어 입력 단계==");
         while(true){
-            System.out.println("==단어 입력 단계==");
             System.out.print("영어 단어를 입력하세요 (종료는 'q'): ");
-
             String englishWord = sc.nextLine();
+
             if(englishWord.equals("q")){
                 break;
             }
@@ -27,9 +27,8 @@ public class DictionaryTest {
         }
 
         System.out.println();
-
+        System.out.println("==단어 검색 단계==");
         while(true){
-            System.out.println("==단어 검색 단계==");
             System.out.print("찾을 영어 단어를 입력하세요 (종료는 'q'): ");
 
             String englishWord = sc.nextLine();
