257763be633e1034431686ee699269bb44aaa6c0
Hyunoh_Na <ohmap@naver.com>
2025-03-29 16:33:00 +0900

    3/29

--- src/collection/deque/test/stack/BrowserHistory.java
+++ src/collection/deque/test/stack/BrowserHistory.java
@@ -5,25 +5,25 @@ import java.util.Deque;
 
 public class BrowserHistory {
 
-    private Deque<String> browser = new ArrayDeque<>();
+    private Deque<String> history = new ArrayDeque<>();
     private String currentPage = null;
+
 
     public void visitPage(String url) {
         if(currentPage != null){
-            browser.push(currentPage);
+            history.push(currentPage);
         }
         currentPage = url;
         System.out.println("방문: " + url);
     }
 
     public String goBack() {
-        if(!browser.isEmpty()){
-            currentPage = browser.pop();
+        if(!history.isEmpty()){
+            currentPage = history.pop();
             System.out.println("뒤로 가기: " + currentPage);
             return currentPage;
         }
         return null;
-        //System.out.println("뒤로 가기: " + browser.peek());
-        //return browser.peek();
+
     }
 }
